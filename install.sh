23c583298f8e8c6aac2962813956b3f55739bfb8
MacBook-Air <macbook-air@MacBook-Air-Devops.local>
2023-12-15 14:36:06 +0700

    fix

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-sudo wget https://raw.githubusercontent.com/xNative/kong-install/blob/main/kong-install.conf  > /dev/null  2>&1
+wget https://raw.githubusercontent.com/xNative/kong-install/main/kong-install.conf  > /dev/null  2>&1
 
-sudo wget https://raw.githubusercontent.com/xNative/kong-install/blob/main/kong-install.sh  > /dev/null  2>&1
+sudo wget https://raw.githubusercontent.com/xNative/kong-install/main/kong-install.sh  > /dev/null  2>&1
 
 sudo printf 'cp\n' | sudo bash kong-install.sh -v 3.4.2 -p kong  > /dev/null  2>&1
 
