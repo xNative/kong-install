ad331c167aeee01034b3f6ec6e5e9dd574de1002
xNative <111569724+xNative@users.noreply.github.com>
2024-01-26 09:19:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-wget https://raw.githubusercontent.com/xNative/kong-install/main/kong-install.conf  > /dev/null  2>&1
+sudo wget https://raw.githubusercontent.com/xNative/kong-install/main/kong-install.conf  > /dev/null  2>&1
 
 sudo wget https://raw.githubusercontent.com/xNative/kong-install/main/kong-install.sh  > /dev/null  2>&1
 
-sudo printf 'cp\n' | sudo bash kong-install.sh -v 3.4.2 -p kong  > /dev/null  2>&1
+sudo printf 'st\n' | sudo bash kong-install.sh -v 3.4.2 -p kong  > /dev/null  2>&1
 
 sudo rm kong-install.conf
 
